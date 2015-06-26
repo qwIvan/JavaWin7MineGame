9c2fba23763676b9372a9e8e24c2aff42aaa1357
Ivan <qw7692336@gmail.com>
2015-06-26 11:37:24 +0800

    改进换行

--- src/ivan/mine/Panel.java
+++ src/ivan/mine/Panel.java
@@ -81,8 +81,9 @@ public class Panel extends JPanel implements MouseListener{
 		if(e.getButton()==1&&e.getClickCount()==2){
 			this.middle();
 		}else if(e.getButton()==2||e.getButton()==1&&
-				(e.getModifiersEx()&InputEvent.BUTTON3_DOWN_MASK)!=0||
-				e.getButton()==3&&(e.getModifiersEx()& InputEvent.BUTTON1_DOWN_MASK)!=0){
+				(e.getModifiersEx()&InputEvent.BUTTON3_DOWN_MASK)!=0
+				||e.getButton()==3&&(e.getModifiersEx()&
+				InputEvent.BUTTON1_DOWN_MASK)!=0){
 			pointer.middleDown(true);
 		}else if(e.getButton()==3){
 			pointer.right();
@@ -91,8 +92,8 @@ public class Panel extends JPanel implements MouseListener{
 	public void mouseReleased(MouseEvent e) {
 		if(pointer==null)return;
 		if(e.getButton()==2||e.getButton()==1&&
-				(e.getModifiersEx()&InputEvent.BUTTON3_DOWN_MASK)!=0||
-				e.getButton()==3&&(e.getModifiersEx()&
+				(e.getModifiersEx()&InputEvent.BUTTON3_DOWN_MASK)!=0
+				||e.getButton()==3&&(e.getModifiersEx()&
 				InputEvent.BUTTON1_DOWN_MASK)!=0){
 			this.middle();
 		}else if(e.getButton()==1){
@@ -101,12 +102,14 @@ public class Panel extends JPanel implements MouseListener{
 	}
 	public void mouseEntered(MouseEvent e) {
 		this.pointer = (Button) e.getSource();
-		if((e.getModifiersEx()&InputEvent.BUTTON2_DOWN_MASK)!=0||(e.getModifiersEx()&
+		if((e.getModifiersEx()&InputEvent.BUTTON2_DOWN_MASK)!=0
+				||(e.getModifiersEx()&
 				(InputEvent.BUTTON1_DOWN_MASK|InputEvent.BUTTON3_DOWN_MASK))
 				==(InputEvent.BUTTON1_DOWN_MASK|InputEvent.BUTTON3_DOWN_MASK)){
 			pointer.middleDown(true);
 		}else{
-			pointer.leftDown((e.getModifiersEx()&InputEvent.BUTTON1_DOWN_MASK)!=0);
+			pointer.leftDown((e.getModifiersEx()
+					&InputEvent.BUTTON1_DOWN_MASK)!=0);
 		}
 	}
 	public void mouseExited(MouseEvent e) {
